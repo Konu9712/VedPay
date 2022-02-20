2a228fcbcdfa3402589ab83ef94f0336ef5250fc
Konu <konarkdave00@gmail.com>
2022-02-20 18:49:37 +0530

    Add Card Detail Screen UI

---
+++ Pages/AddCardScreen/AddCardScreen.js
@@ -0,0 +1,113 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { Button, TextInput,TextInputMask } from 'react-native-paper';
+import ModalConatiner from '../../Components/Logo/Modal';
+
+
+export default function AddCardScreen() {
+ 
+ 
+return (
+<View>
+    <View style={styles.text_container}>
+        <Text style={styles.desc_1}>
+        Add Card
+        </Text>
+    </View>
+
+    <ModalConatiner
+      bulkProps={
+        <>
+            <View style={styles.input_wrapper}>
+            <TextInput
+                label="Card Number"
+                mode="flat"
+                style={styles.card_number}
+                outlineColor="green"
+                activeUnderlineColor="green"
+                placeholder="Card Number"
+                keyboardType="number-pad"
+              
+          />
+            <View style={styles.second_row}>
+            <TextInput
+                label="Valid Till"
+                mode="flat"
+                style={styles.expiry_date}
+                outlineColor="green"
+                activeUnderlineColor="green"
+                placeholder="MM / YY"
+                keyboardType="number-pad"
+              
+          />
+
+            <TextInput
+                label="CVV"
+                mode="flat"
+                style={styles.cvv}
+                outlineColor="green"
+                activeUnderlineColor="green"
+                placeholder="CVV"
+                keyboardType="number-pad"
+              
+          />
+            </View>
+
+            <TextInput
+                label="Name on the card"
+                mode="flat"
+                style={styles.card_holder}
+                outlineColor="green"
+                activeUnderlineColor="green"
+                placeholder="Card Holder"
+                keyboardType="number-pad"
+              
+          />
+
+            <Button mode="contained" 
+               style={styles.btn_getStarted}  
+               onPress={() => console.log("addCard")}
+               color='green'>
+                  Add Card
+                 </Button> 
+
+            </View>
+
+        </>
+    }
+    />
+</View>
+    );
+}
+
+const styles = StyleSheet.create({
+    text_container:{
+        marginTop:hp("10%"),
+    
+    },
+    desc_1:{
+        fontWeight:"bold",
+        fontSize: hp('4%'),
+        alignSelf: 'stretch',
+        textAlign: 'center',
+        color:'#ffff'
+    },
+    second_row:{
+        flexDirection: "row",
+        marginTop:hp("5%")
+    },
+    expiry_date:{
+        width:wp("30%")
+    },
+   cvv:{
+    width:wp("30%"),
+    marginLeft:wp("20%"),
+    },
+    card_holder:{
+        marginTop:hp("5%")
+    },
+    btn_getStarted:{
+        marginTop:hp("5%")
+    }
+});
