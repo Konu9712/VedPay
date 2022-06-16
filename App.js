71f75c07ba6e9e8b002ca0ebdcbfbff9aa6d788e
Konu <51238256+Konu9712@users.noreply.github.com>
2022-06-16 16:35:42 +0530

    Set up Reducer + Thunk

--- App.js
+++ App.js
@@ -1,31 +1,8 @@
 import * as React from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  ImageBackground,
-  Dimensions,
-} from "react-native";
-// import Constants from "expo-constants";
-// import SplashScreen from "./Pages/SplashScreen/SplashScreen";
-// import GettingStartedScreen from "./Pages/GettingStarted/GettingStartedScreen";
-// import Logo from "./Components/Logo/Logo";
-// import OtpScreen from "./Pages/OtpScreen/OtpScreen";
-// import PreQrScreen from "./Pages/PreQrScreen/PreQrScreen";
-// import QRScanner from "./Pages/QRScanner/QRScanner";
-// import CardCollectionScreen from "./Pages/Card/CardCollectionScreen/CardCollectionScreen";
-// import AddCardScreen from "./Pages/Card/AddCardScreen/AddCardScreen";
-// import Modal from "./Components/Modal/Modal";
-// import CardTransactionListScreen from "./Pages/Card/CardTransactionListScreen/CardTransactionListScreen";
-// import AddMoneyMainScreen from "./Pages/In/AddMoneyMainScreen/AddMoneyMainScreen";
-// import AddMoneySource from "./Pages/In/AddMoneySource/AddMoneySource";
-// import AddMoneySuccessfully from "./Pages/AddMoneySuccessfully/AddMoneySuccessfully";
-// import HistoryMainScreen from "./Pages/HistoryMainScreen/HistoryMainScreen";
-// import HistoryDetails from "./Pages/History/HistoryDetails/HistoryDetails";
-// import OutMainScreen from "./Pages/Out/OutMainScreen/OutMainScreen";
-// import OutContactChatScreen from "./Pages/OutContactChatScreen/OutContactChatScreen";
-// import OutPayScreen from "./Pages/Out/OutPayScreen/OutPayScreen";
+import { StyleSheet, ImageBackground, Dimensions } from "react-native";
+import { Provider } from "react-redux";
 import Naviagation from "./Naviagation/Naviagation";
+import store from "./store/store";
 
 const d = Dimensions.get("window");
 export default function App() {
@@ -35,34 +12,10 @@ export default function App() {
       source={require("./assets/background.png")}
       imageStyle={{ resizeMode: "repeat" }}
     >
-      {/* <SplashScreen /> */}
-      {/* <GettingStartedScreen /> */}
-      {/* <OtpScreen /> */}
-      {/* <PreQrScreen /> */}
-      {/* <QRScanner /> */}
-      {/* Card Collection ---------------------- */}
-      {/* <CardCollectionScreen /> */}
-      {/* <AddCardScreen /> */}
-      {/* <Modal /> */}
-      {/* <CardTransactionListScreen /> */}
-      {/* IN------------------------------------ */}
-      {/* <AddMoneyMainScreen /> */}
-      {/* <AddMoneySource /> */}
-      {/* <AddMoneySuccessfully /> */}
-      {/* History-------------------------------- */}
-      {/* <HistoryMainScreen /> */}
-      {/* <HistoryDetails /> */}
-      {/* OUT------------------------------------ */}
-      {/* <OutMainScreen /> */}
-      {/* <OutContactChatScreen /> */}
-      {/* <OutPayScreen /> */}
-      <Naviagation />
+      <Provider store={store}>
+        <Naviagation />
+      </Provider>
     </ImageBackground>
-
-    // <Naviagation />
-    // <>
-    //   <Text>HEllo</Text>
-    // </>
   );
 }
 
